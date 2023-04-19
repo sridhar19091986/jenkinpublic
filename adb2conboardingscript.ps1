912c2196bd5b15e483e09a594ac8b4cc2a57f4a6
sridhar19091986 <mywayattech@gmail.com>
2023-04-20 00:31:03 +0530

    Update adb2conboardingscript.ps1

--- adb2conboardingscript.ps1
+++ adb2conboardingscript.ps1
@@ -17,5 +17,11 @@ Import-Module Microsoft.Graph.Applications
 $params = @{
 	displayName = $AppName
 }
-Connect-MgGraph -ClientCredential $env:GRAPH_CLIENT_ID -ClientSecret $env:GRAPH_CLIENT_SECRET -TenantId $env:GRAPH_TENANT_ID
+ [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $body = @{grant_type = "client_credentials"; scope = "https://graph.microsoft.com/.default"; client_id = $env:GRAPH_CLIENT_ID; client_secret = $env:GRAPH_CLIENT_SECRET }
+    $response = Invoke-RestMethod -Uri https://login.microsoftonline.com/$env:GRAPH_TENANT_ID/oauth2/v2.0/token -Method Post -Body $body
+    $token = $response.access_token
+Write-Host "Parameter 7: $token"					
+    Connect-MgGraph -AccessToken $token -ErrorAction Stop
+# Connect-MgGraph -ClientCredential $env:GRAPH_CLIENT_ID -ClientSecret $env:GRAPH_CLIENT_SECRET -TenantId $env:GRAPH_TENANT_ID
 New-MgApplication -BodyParameter $params
