e413875cd8c86300d31493ff8853668dc0984de8
sridhar19091986 <mywayattech@gmail.com>
2023-04-20 00:11:33 +0530

    Update adb2conboardingscript.ps1

--- adb2conboardingscript.ps1
+++ adb2conboardingscript.ps1
@@ -14,5 +14,5 @@ Import-Module Microsoft.Graph.Applications
 $params = @{
 	displayName = $AppName
 }
-
+Connect-MgGraph -ClientCredential $env:GRAPH_CLIENT_ID -ClientSecret $env:GRAPH_CLIENT_SECRET -TenantId $env:GRAPH_TENANT_ID
 New-MgApplication -BodyParameter $params
