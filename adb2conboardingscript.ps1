dfa90f20b109f577717e9dc59672cd3772e71484
sridhar19091986 <mywayattech@gmail.com>
2023-04-19 18:07:27 +0530

    Create adb2conboardingscript.ps1

---
+++ adb2conboardingscript.ps1
@@ -0,0 +1,11 @@
+# PowerShell script.ps1
+
+param (
+  [string]$Env,
+  [string]$Version,
+  [string]$STRINGPARAMETER
+)
+
+Write-Host "Parameter 1: $Env"
+Write-Host "Parameter 2: $Version"
+Write-Host "Parameter 3: $STRINGPARAMETER" 
