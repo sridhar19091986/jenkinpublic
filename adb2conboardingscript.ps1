1789c9d3e3100d57adf4f72e0dff46744126e3e4
sridhar19091986 <mywayattech@gmail.com>
2023-04-19 23:03:32 +0530

    Update adb2conboardingscript.ps1

--- adb2conboardingscript.ps1
+++ adb2conboardingscript.ps1
@@ -9,3 +9,11 @@ param (
 Write-Host "Parameter 1: $Env"
 Write-Host "Parameter 2: $Version"
 Write-Host "Parameter 3: $AppName" 
+
+Import-Module Microsoft.Graph.Applications
+
+$params = @{
+	displayName = $AppName
+}
+
+New-MgApplication -BodyParameter $params
